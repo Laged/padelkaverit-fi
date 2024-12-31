24b9b406fb1a885df190e5291476afac16ce976e
Matti Parkkila <parkkila.matti@gmail.com>
2024-12-31 10:35:47 +0200

    feat: initial threejs color circles

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,15 +1,15 @@
 import { FC } from "react";
 import DarkModeToggle from "@/components/DarkModeToggle";
-import AnimatedSvg from "@/components/atoms/AnimatedSvg";
+import AnimatedSvgThree from "./atoms/AnimatedSvgThree";
 
 const HeroSection: FC = () => {
   return (
-    <section className="flex min-h-screen w-full bg-main dark:bg-background transition-colors duration-500 flex flex-col items-center justify-center relative p-4">
-      <div className="absolute top-4 right-4">
+    <section className="fixed inset-0 w-screen h-screen overflow-hidden bg-main dark:bg-background transition-colors duration-500">
+      <div className="absolute top-4 right-4 z-10">
         <DarkModeToggle />
       </div>
       <div className="w-full h-full">
-        <AnimatedSvg />
+        <AnimatedSvgThree />
       </div>
     </section>
   );
